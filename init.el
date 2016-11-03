fd9de995a01a7aee4879ab7cb65f396568759ca8
Xiaoxi Guo <xguo0122@gmail.com>
2016-11-03 00:07:30 -0700

    add init-packages

--- init.el
+++ init.el
@@ -1,51 +1,21 @@
+;;; This file bootstraps the configuration
 
-;; Using Melpa
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (package-initialize)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-  )
-(require 'cl)
-;; Add whatever packages you want here
-(defvar xiaoxig/packages '(
-			   company
-			   monokai-theme
-			   hungry-delete
-			   swiper
-			   counsel
-			   smartparens
-			   js2-mode
-			   nodejs-repl
-			   exec-path-from-shell
-			   popwin
 
-			   ) "Default packages")
-(setq package-selected-packages xiaoxig/packages)
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
 
-(defun xiaoxig/packages-installed-p ()
-  (loop for pkg in xiaoxig/packages
-	when (not (package-installed-p pkg)) do (return nil)
-	finally (return t)))
+(add-to-list 'load-path "~/.emacs.d/lisp")
 
-(unless (xiaoxig/packages-installed-p)
-  (message "%s" "Refreshing packages database")
-  (package-refresh-contents)
-  (dolist (pkg xiaoxig/packages)
-    (when (not (package-installed-p pkg))
-      (package-install pkg))))
-
-;; config for exec-path-from-shell
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
-
-;; Load monokai theme
-(load-theme 'monokai t)
+(require 'init-packages)
 
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
 (global-linum-mode t)
-(global-company-mode t)
+
 (global-hl-line-mode t)
 (delete-selection-mode t)
 (global-auto-revert-mode t)
@@ -58,6 +28,7 @@
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 (setq inhibit-splash-screen t)
+(setq ring-bell-function 'ignore)
 
 ;; recent files
 (require 'recentf)
@@ -68,34 +39,12 @@
 ;; Full-screen
 (setq initial-frame-alist (quote ((fullscreen . maximized))))
 
-;; Parent
-(require 'smartparens)
-;;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
-(smartparens-global-mode t)
-
-;; Hungry-delete mode
-(require 'hungry-delete)
-(global-hungry-delete-mode)
-
-;; config for ivy, counsel, and swiper
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
 (global-set-key "\C-s" 'swiper)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-h f") 'counsel-describe-function)
 (global-set-key (kbd "C-h v") 'counsel-describe-variable)
-
-;; config for js
-(setq auto-mode-alist
-      (append
-       '(("\\.js\\'" . js2-mode))
-       auto-mode-alist))
-
-;; add popwin mode
-(require 'popwin)
-(popwin-mode t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
---
+++ lisp/init-packages.el
@@ -0,0 +1,68 @@
+;; Using Melpa
+(when (>= emacs-major-version 24)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+  )
+(require 'cl)
+;; Add whatever packages you want here
+(defvar xiaoxig/packages '(
+			   company
+			   monokai-theme
+			   hungry-delete
+			   swiper
+			   counsel
+			   smartparens
+			   js2-mode
+			   nodejs-repl
+			   exec-path-from-shell
+			   popwin
+
+			   ) "Default packages")
+
+(setq package-selected-packages xiaoxig/packages)
+
+(defun xiaoxig/packages-installed-p ()
+  (loop for pkg in xiaoxig/packages
+	when (not (package-installed-p pkg)) do (return nil)
+	finally (return t)))
+
+(unless (xiaoxig/packages-installed-p)
+  (message "%s" "Refreshing packages database")
+  (package-refresh-contents)
+  (dolist (pkg xiaoxig/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+;; config for company
+(global-company-mode t)
+
+;; Load monokai theme
+(load-theme 'monokai t)
+
+;; Hungry-delete mode
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+
+;; config for ivy, counsel, and swiper
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+
+;; Parent
+(require 'smartparens)
+;;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
+(smartparens-global-mode t)
+
+;; config for js
+(setq auto-mode-alist
+      (append
+       '(("\\.js\\'" . js2-mode))
+       auto-mode-alist))
+
+;; config for exec-path-from-shell
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+;; add popwin mode
+(require 'popwin)
+(popwin-mode t)
+
+(provide 'init-packages)
