cd93d9196f2286e7e56716ced4cdfcce6169be3a
Xiaoxi Guo <xguo0122@gmail.com>
2016-11-16 22:30:56 -0800

    fix smartparent problem.

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -50,6 +50,7 @@
 ;; Parent
 ;;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
 (smartparens-global-mode t)
+(sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
 
 ;; config for js
 (setq auto-mode-alist
